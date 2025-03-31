3f8f014af2d4c13ab78e57dc103f44dc5a31b322
amies <amies.shen@gmail.com>
2025-03-31 14:10:31 +0800

    test: 测试

--- packages/component-library/src/index.ts
+++ packages/component-library/src/index.ts
@@ -1,4 +1,5 @@
+//增加组件库注释
 export { default as Button } from './components/Button/Button'
 export { default as Input } from './components/Input/Input'
-export { default as Modal } from './components/Modal/Modal'
+export { default as Modal } from './components/Modal/Modal';
 export { defaultTheme } from './styles/theme'
